c17aa13a368628f4a2bf4e8f1f025755a21ddbd9
shrikant kallshetty <kalshettyshrikant@gmail.com>
2023-04-01 02:28:02 +0530

    feat: added some more filters, datasets, styling

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -21,44 +21,114 @@ function Chart(props) {
         data: data.map((d) => d.likelihood),
         fill: false,
         borderColor: '#2196f3',
-        tension: 0.1
+        tension: 0.1,
       },
       {
         label: 'Relevance',
         data: data.map((d) => d.relevance),
         fill: false,
         borderColor: '#4caf50',
-        tension: 0.1
-      }
-    ]
+        tension: 0.1,
+      },
+      {
+        label: 'Year',
+        data: data.map((d) => d.year),
+        fill: false,
+        borderColor: '#9c27b0',
+        tension: 0.1,
+      },
+      {
+        label: 'Country',
+        data: data.map((d) => d.country),
+        fill: false,
+        borderColor: '#e91e63',
+        tension: 0.1,
+      },
+      {
+        label: 'Topics',
+        data: data.map((d) => d.topic),
+        fill: false,
+        borderColor: '#607d8b',
+        tension: 0.1,
+      },
+      {
+        label: 'Region',
+        data: data.map((d) => d.region),
+        fill: false,
+        borderColor: '#ff9800',
+        tension: 0.1,
+      },
+      {
+        label: 'City',
+        data: data.map((d) => d.city),
+        fill: false,
+        borderColor: '#795548',
+        tension: 0.1,
+      },
+    ],
   };
 
   const barChartData = {
-    labels: data.map((d) => d.end_year),
-    datasets: [
-      {
-        label: 'Intensity',
-        data: data.map((d) => d.intensity),
-        backgroundColor: 'rgba(244, 67, 54, 0.5)',
-        borderColor: '#f44336',
-        borderWidth: 1,
-      },
-      {
-        label: 'Likelihood',
-        data: data.map((d) => d.likelihood),
-        backgroundColor: 'rgba(33, 150, 243, 0.5)',
-        borderColor: '#2196f3',
-        borderWidth: 1,
-      },
-      {
-        label: 'Relevance',
-        data: data.map((d) => d.relevance),
-        backgroundColor: 'rgba(76, 175, 80, 0.5)',
-        borderColor: '#4caf50',
-        borderWidth: 1,
-      }
-    ]
-  };
+  labels: data.map((d) => d.end_year),
+  datasets: [
+    {
+      label: 'Intensity',
+      data: data.map((d) => d.intensity),
+      backgroundColor: 'rgba(244, 67, 54, 0.5)',
+      borderColor: '#f44336',
+      borderWidth: 1,
+    },
+    {
+      label: 'Likelihood',
+      data: data.map((d) => d.likelihood),
+      backgroundColor: 'rgba(33, 150, 243, 0.5)',
+      borderColor: '#2196f3',
+      borderWidth: 1,
+    },
+    {
+      label: 'Relevance',
+      data: data.map((d) => d.relevance),
+      backgroundColor: 'rgba(76, 175, 80, 0.5)',
+      borderColor: '#4caf50',
+      borderWidth: 1,
+    },
+    {
+      label: 'Year',
+      data: data.map((d) => d.end_year),
+      backgroundColor: 'rgba(255, 193, 7, 0.5)',
+      borderColor: '#ffc107',
+      borderWidth: 1,
+    },
+    {
+      label: 'Country',
+      data: data.map((d) => d.country),
+      backgroundColor: 'rgba(156, 39, 176, 0.5)',
+      borderColor: '#9c27b0',
+      borderWidth: 1,
+    },
+    {
+      label: 'Topics',
+      data: data.map((d) => d.topics),
+      backgroundColor: 'rgba(0, 150, 136, 0.5)',
+      borderColor: '#009688',
+      borderWidth: 1,
+    },
+    {
+      label: 'Region',
+      data: data.map((d) => d.region),
+      backgroundColor: 'rgba(63, 81, 181, 0.5)',
+      borderColor: '#3f51b5',
+      borderWidth: 1,
+    },
+    {
+      label: 'City',
+      data: data.map((d) => d.city),
+      backgroundColor: 'rgba(255, 152, 0, 0.5)',
+      borderColor: '#ff9800',
+      borderWidth: 1,
+    }
+  ]
+};
 
   return (
     <>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,53 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import Chart from '../components/Chart';
 import Filter from '../components/Filter';
+import '../App.css'
 
 function Dashboard() {
-  const [data, setData] = useState([]);
-  const [yearFilter, setYearFilter] = useState('');
-  const [topicFilter, setTopicFilter] = useState('');
-  const [regionFilter, setRegionFilter] = useState('');
-  const [error, setError] = useState('');
+const [data, setData] = useState([]);
+const [yearFilter, setYearFilter] = useState('');
+const [topicFilter, setTopicFilter] = useState('');
+const [regionFilter, setRegionFilter] = useState('');
+const [sectorFilter, setSectorFilter] = useState('');
+const [pestFilter, setPestFilter] = useState('');
+const [sourceFilter, setSourceFilter] = useState('');
+const [swotFilter, setSwotFilter] = useState('');
+const [countryFilter, setCountryFilter] = useState('');
+const [cityFilter, setCityFilter] = useState('');
+const [error, setError] = useState('');
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/data')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => setError(error.message));
-  }, []);
-  console.log(data)
-  console.log(yearFilter,"yearFilter")
-  console.log(topicFilter,"topicFiter")
-  console.log(regionFilter,"regionFilter")
-  
-  const years = [...new Set(data.map(item => +item.end_year))];
-  const topics = [...new Set(data.map(item => item.topic))];
-  const regions = [...new Set(data.map(item => item.region))];
+useEffect(() => {
+fetch('http://localhost:5000/api/data')
+.then(response => response.json())
+.then(data => setData(data))
+.catch(error => setError(error.message));
+}, []);
+console.log(data)
+const years = [...new Set(data.map(item => +item.end_year))];
+const topics = [...new Set(data.map(item => item.topic))];
+const regions = [...new Set(data.map(item => item.region))];
+const sectors = [...new Set(data.map(item => item.sector))];
+const pests = [...new Set(data.map(item => item.pest))];
+const sources = [...new Set(data.map(item => item.source))];
+const swots = [...new Set(data.map(item => item.swot))];
+const countries = [...new Set(data.map(item => item.country))];
+const cities = [...new Set(data.map(item => item.city))];
 
+const filteredData = data.filter(item => {
+return (
+(!yearFilter || item.end_year === Number(yearFilter)) &&
+(!topicFilter || item.topic === topicFilter) &&
+(!regionFilter || item.region === regionFilter) &&
+(!sectorFilter || item.sector === sectorFilter) &&
+(!pestFilter || item.pest === pestFilter) &&
+(!sourceFilter || item.source === sourceFilter) &&
+(!swotFilter || item.swot === swotFilter) &&
+(!countryFilter || item.country === countryFilter) &&
+(!cityFilter || item.city === cityFilter)
+);
+});
 
-console.log(years,"years")
-  const filteredData = data.filter(item => {
-    return (
-      (!yearFilter || item.end_year === Number(yearFilter)) &&
-      (!topicFilter || item.topic === topicFilter) &&
-      (!regionFilter || item.region === regionFilter)
-    );
-  });
+return (
+<div className="dashboard">
+<h2>Dashboard</h2>
+{error && <p className="error">{error}</p>}
+<div className="filters">
+<Filter className="filters" label="Year" options={years} selectedOption={Number(yearFilter)} onChange={e => setYearFilter(e.target.value)} />
+<Filter className="filters" label="Topic" options={topics} selectedOption={topicFilter} onChange={e => setTopicFilter(e.target.value)} />
+<Filter className="filters" label="Region" options={regions} selectedOption={regionFilter} onChange={e => setRegionFilter(e.target.value)} />
+<Filter className="filters" label="Sector" options={sectors} selectedOption={sectorFilter} onChange={e => setSectorFilter(e.target.value)} />
+<Filter className="filters" label="PEST" options={pests} selectedOption={pestFilter} onChange={e => setPestFilter(e.target.value)} />
+<Filter className="filters" label="Source" options={sources} selectedOption={sourceFilter} onChange={e => setSourceFilter(e.target.value)} />
+<Filter className="filters" label="SWOT" options={swots} selectedOption={swotFilter} onChange={e => setSwotFilter(e.target.value)} />
+<Filter className="filters" label="Country" options={countries} selectedOption={countryFilter} onChange={e => setCountryFilter(e.target.value)} />
+<Filter className="filters" label="City" options={cities} selectedOption={cityFilter} onChange={e => setCityFilter(e.target.value)} />
 
-  return (
-    <div className="dashboard">
-      <h2>Dashboard</h2>
-      {error && <p className="error">{error}</p>}
-      <div className="filters">
-        <Filter label="Year" options={years} selectedOption={Number(yearFilter)} onChange={e => setYearFilter(e.target.value)} />
-        <Filter label="Topic" options={topics} selectedOption={topicFilter} onChange={e => setTopicFilter(e.target.value)} />
-        <Filter label="Region" options={regions} selectedOption={regionFilter} onChange={e => setRegionFilter(e.target.value)} />
-      </div>
-      <Chart data={filteredData} />
-    </div>
-  );
+</div>
+<Chart data={filteredData} />
+</div>
+);
 }
-
 export default Dashboard;
+
 
 
